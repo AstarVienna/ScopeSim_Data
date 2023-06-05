8b42167111e302a920ed2c3994700ea16d87bc2a
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-06-05 15:15:53 +0200

    Move data directory to be within the package

--- scopesim_data/__init__.py
+++ scopesim_data/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 path_here = Path(__file__).parent
 
-dir_scopesim_cache = path_here.parent / "data"
+dir_scopesim_cache = path_here / "data"
 
 __all__ = [
     dir_scopesim_cache
